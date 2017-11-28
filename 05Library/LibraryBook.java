5b02c3818fe2902b68a73e56a2018e0be822bb5c
DamianWasilewicz <dwasilewicz@stuy.edu>
2017-11-28 14:42:23 -0500

    Created LibraryBook class

---
+++ 05Library/LibraryBook.java
@@ -0,0 +1,25 @@
+public abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
+    private String callNumber;
+    public LibraryBook(String Author, String Title, String ISBN, String callNumber){
+	super(Author, Title, ISBN);
+	this.callNumber = callNumber;
+    }
+    public String getcallNumber(){
+	return callNumber;
+    }
+    public void setcallNumber(String callNumber){
+	this.callNumber = callNumber;
+    }
+    public abstract void checkout(String patron, String due){
+    }
+    public abstract void returned(){
+    }
+    public abstact String circulationStatus(){
+    }
+    public int compareTo(LibraryBook other){
+	return this.getcallNumber().compareTo(other.getcallNumber());
+    }
+    public String toString(){
+	super.toString + ", " + callNumber + ", " + circulationStatus;
+    }
+}
