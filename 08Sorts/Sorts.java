33fdc36813f73c5da8b3e1f26933204dbfdbc21e
Damian Wasilewicz <dwasilewicz@stuy.edu>
2017-12-07 10:09:06 -0500

    Updated Insertion sort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -2,6 +2,11 @@ public class Sorts{
   public static String name(){
     return "09.Wasilewicz.Damian";
   }
+  public static void swap(int[] ary, int a, int b){
+    int c = ary[a];
+    ary[a] = ary[b];
+    ary[b] = c;
+  }
   public static void selectionSort(int[] data){
         int where = 0;
         for(int counter = 0; counter<data.length; counter++){
@@ -17,17 +22,12 @@ public class Sorts{
         }
       }
   public static void insertionSort(int[] data){
-    int where = 0;
-    for(int counter = 0; counter<data.length; counter++){
-      int min = data[counter];
-      for(int counter1 = counter; counter1<data.length; counter1++){
-        if (data[counter1] < min){
-          min = data[counter1];
-          where = counter1;
-        }
-        data[where] = data[counter];
-        data[counter] = min;
+  for(int counter = 0; counter<data.length - 1; counter++){
+    for(int counter1 = counter + 1; counter1 > 0; counter1--){
+      if(data[counter1] < data[counter1 -1]){
+        swap(data, counter1 - 1, counter1);
       }
     }
   }
-  }
+      }
+    }
