0b9457e68c66da05367fe6f50f16ed3d5eecc796
Damian Wasilewicz <dwasilewicz@stuy.edu>
2017-12-09 18:04:03 -0500

    Added BubbleSort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Sorts{
   public static String name(){
     return "09.Wasilewicz.Damian";
@@ -8,26 +9,40 @@ public class Sorts{
     ary[b] = c;
   }
   public static void selectionSort(int[] data){
-        int where = 0;
-        for(int counter = 0; counter<data.length; counter++){
-          int min = data[counter];
-          for(int counter1 = counter; counter1<data.length; counter1++){
-            if (data[counter1] < min){
-              min = data[counter1];
-              where = counter1;
-            }
-            data[where] = data[counter];
-            data[counter] = min;
-          }
+    for(int counter = 0; counter < data.length; counter++){
+      for(int counter1 = counter; counter1 < data.length; counter1++){
+        if(data[counter] > data[counter1]){
+        swap(data, counter, counter1);
         }
-      }
-  public static void insertionSort(int[] data){
-  for(int counter = 0; counter<data.length - 1; counter++){
-    for(int counter1 = counter + 1; counter1 > 0; counter1--){
-      if(data[counter1] < data[counter1 -1]){
-        swap(data, counter1 - 1, counter1);
-      }
+       }
     }
   }
-      }
+ public static void insertionSort(int[] data){
+    for(int counter = 0; counter < data.length - 1; counter++){
+      for(int counter1 = counter; counter1 > 0; counter1--){
+        if(data[counter1] < data[counter1 -1]){
+        swap(data, counter1 - 1, counter1);
+        }
+       }
     }
+  }
+  public static void bubbleSort(int[] data){
+    for(int counter = 0; counter < data.length; counter++){
+      for(int counter1 = 0; counter1 < data.length - counter - 1; counter1++){
+        if(data[counter1] > data[counter1 + 1]){
+        swap(data, counter1, counter1 + 1);
+        }
+       }
+     }
+  }
+  public static void main(String[]artie){
+       int[] randish = new int[15];
+       for(int i = 0 ; i < randish.length; i++){
+           randish[i] =(int)(Math.random()*100);
+       }
+       System.out.println(name());
+       System.out.println(Arrays.toString(randish));
+       bubbleSort(randish);
+       System.out.println(Arrays.toString(randish));
+   }
+}
