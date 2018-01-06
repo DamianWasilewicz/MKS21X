735e814469b85c0c3bfcd4a7cb92ba51053c6297
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-01-06 18:04:22 -0500

    Fixed SuperArrayIterator

--- 06SuperArrayIterator/SuperArray.java
+++ 06SuperArrayIterator/SuperArray.java
@@ -1,4 +1,5 @@
-public class SuperArray{
+import java.util.Iterator;
+public class SuperArray implements Iterable<String>{
   private String[] data;
   private int size;
   //Constructor constructs a blank list with a potential 10 spots;
@@ -9,149 +10,164 @@ public class SuperArray{
     }
     size = 0;
   }
-  //Clears entire list, setting every element to null;
-  public void clear(){
-    for(int counter = 0; counter < data.length; counter++){
-      data[counter] = "";
-    }
-    size = 0;
-  }
-  //returns the number of non-null elements in the array;
- public int size(){
-   return size;
- }
- //checks if the array contains non-null elements;
- public boolean isEmpty(){
-   return size == 0;
- }
- //appends an element to the end of the list, calling resize if needed;
- public boolean add(String element){
-   if(size == data.length){
-  resize();
+  //Constructor, creates a blank list with starting length startinCapacity;
+ public SuperArray(int startingCapacity){
+   data = new String[startingCapacity];
+   for (int counter = 0; counter < data.length; counter++){
+     data[counter] = "";
    }
-   data[size] = element;
-   size++;
-   return true;
+   size = 0;
  }
- //used to print out the list;
- public String toString(){
-   String result = "[";
-  for(int counter = 0; counter < size; counter++){
-    result+= data[counter];
-    if(counter != size - 1){
-      result+= ", ";
-    }
-  }
-  result+= "]";
-  return result;
+ // constructs new iterator (SuperArrayIterator);
+ public Iterator<String> iterator(){
+   return new SuperArrayIterator(this);
+ }
+ //Clears entire list, setting every element to null;
+ public void clear(){
+   for(int counter = 0; counter < data.length; counter++){
+     data[counter] = "";
+   }
+   size = 0;
+ }
+ //returns the number of non-null elements in the array;
+public int size(){
+  return size;
 }
- //returns the element at a certain index of the list;
- public String get(int index){
-   if (index < 0 || index >= size){
-     System.out.println("WRONG INDEX, YOU FOOL");
-     return null;
-   }
-   else{
-     return data[index];
+//checks if the array contains non-null elements;
+public boolean isEmpty(){
+  return size == 0;
+}
+//appends an element to the end of the list, calling resize if needed;
+public boolean add(String element){
+  if(size == data.length){
+ resize();
+  }
+  data[size] = element;
+  size++;
+  return true;
+}
+//used to print out the list;
+public String toString(){
+  String result = "[";
+ for(int counter = 0; counter < size; counter++){
+   result+= data[counter];
+   if(counter != size - 1){
+     result+= ", ";
    }
  }
- //sets an element at a certain index to another element of choice;
- public String set(int index, String element){
+ result+= "]";
+ return result;
+}
+//returns the element at a certain index of the list;
+public String get(int index){
   if (index < 0 || index >= size){
-   System.out.println("WRONG INDEX, YOU FOOL");
-   return null;
+    throw new IndexOutOfBoundsException();
   }
-  else {
-  String returned = data[index];
-   data[index] = element;
-   return returned;
- }
- }
- //doubles the size of the superarray for accomodation purposes;
- private void resize(){
-   String[] newdata = new String[data.length * 2];
-   for(int counter = 0; counter < size; counter++){
-     newdata[counter] = data[counter];
-   }
-   for(int counter2 = size; counter2 < data.length; counter2++){
-     newdata[counter2] = "";
-   }
-   data = newdata;
- }
- //checks if a list contains a certain target string;
- public boolean contains(String target){
-   for(int counter = 0; counter < size; counter++){
-     if(data[counter].equals(target)){
-       return true;
-     }
-   }
-   return false;
- }
- //returns the index of the first instance of an element;
- public int indexOf(String target){
-   for(int counter = 0; counter < size; counter++){
-     if(data[counter].equals(target)){
-       return counter;
-     }
-   }
-   return -1;
- }
- //returns the last index of an element;
- public int lastIndexOf(String target){
-   for(int counter = size - 1; counter > 0; counter--){
-     if(data[counter].equals(target)){
-       return counter;
-     }
-   }
-   return -1;
- }
- //inserts an element into a string, bumping elements accordingly;
- public void add(int index, String element){
-   if (index < 0 || size < index){
-     System.out.println("WRONG INDEX YOU FOOL");
-   }
-   else {
-     for(int counter = size; counter >= index; counter--){
-       data[counter + 1] = data[counter];
-     }
-     data[index] = element;
-     size++;
-   }
- }
- //removes an element from a list at a certain index, shifts list accordingly;
- public String remove(int index){
-    if (index < 0 || index >= size){
-      System.out.println("WRONG INDEX YOU FOOL");
-      return null;
+  else{
+    return data[index];
   }
-  String[] answer = new String[data.length];
+}
+//sets an element at a certain index to another element of choice;
+public String set(int index, String element){
+ if (index < 0 || index >= size){
+  throw new IndexOutOfBoundsException();
+ }
+ else {
+ String returned = data[index];
+  data[index] = element;
+  return returned;
+}
+}
+//doubles the size of the superarray for accomodation purposes;
+private void resize(){
+  String[] newdata = new String[data.length * 2];
+  for(int counter = 0; counter < size; counter++){
+    newdata[counter] = data[counter];
+  }
+  for(int counter2 = size; counter2 < data.length; counter2++){
+    newdata[counter2] = "";
+  }
+  data = newdata;
+}
+//checks if a list contains a certain target string;
+public boolean contains(String target){
+  for(int counter = 0; counter < size; counter++){
+    if(data[counter].equals(target)){
+      return true;
+    }
+  }
+  return false;
+}
+//returns the index of the first instance of an element;
+public int indexOf(String target){
+  for(int counter = 0; counter < size; counter++){
+    if(data[counter].equals(target)){
+      return counter;
+    }
+  }
+  return -1;
+}
+//returns the last index of an element;
+public int lastIndexOf(String target){
+  for(int counter = size - 1; counter > 0; counter--){
+    if(data[counter].equals(target)){
+      return counter;
+    }
+  }
+  return -1;
+}
+//inserts an element into a string, bumping elements accordingly;
+public void add(int index, String element){
+if (index < 0 || index > size()){
+  throw new IndexOutOfBoundsException();
+}
+if (data.length == size()){
   resize();
-  for(int counter = 0; counter < index; counter++){
-    answer[counter] = data[counter];
-  }
-  for(int counter2 = index; counter2 < size - 1; counter2++){
-    answer[counter2] = data[counter2 + 1];
-  }
- String removed = data[index];
-  data = answer;
-  size--;
-  return removed;
+}
+String[] data2 = new String[data.length];
+for (int counter = 0; counter < index; counter++){
+  data2[counter] = data[counter];
+}
+data2[index] = element;
+for (int counter2 = index; counter2 < size(); counter2++){
+  data2[counter2 + 1] = data[counter2];
+}
+data = data2;
+size = size + 1;
+}
+//removes an element from a list at a certain index, shifts list accordingly;
+public String remove(int index){
+   if (index < 0 || index >= size){
+   throw new IndexOutOfBoundsException();
+ }
+ String[] answer = new String[data.length];
+ resize();
+ for(int counter = 0; counter < index; counter++){
+   answer[counter] = data[counter];
+ }
+ for(int counter2 = index; counter2 < size - 1; counter2++){
+   answer[counter2] = data[counter2 + 1];
+ }
+String removed = data[index];
+ data = answer;
+ size--;
+ return removed;
 }
 //removes the first instance of a certain element and shifts list accordingly;
 public boolean remove(String element){
-  if(!contains(element)) {
-    return false;
-  }
-  else{
-    int indx = indexOf(element);
-    String inx = data[indx];
-    String[] answer = new String[size - indx + 1];
-    int var =0;
-    for(int counter = indx + 1; counter< size; counter++){
-      answer[indx] = data[counter];
-      var++;
-    }
-    return true;
-    }
-  }
+ if(!contains(element)) {
+   return false;
+ }
+ else{
+   int indx = indexOf(element);
+   String inx = data[indx];
+   String[] answer = new String[size - indx + 1];
+   int var =0;
+   for(int counter = indx + 1; counter< size; counter++){
+     answer[indx] = data[counter];
+     var++;
+   }
+   return true;
+   }
+ }
 }
